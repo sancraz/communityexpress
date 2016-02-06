62c0b319695b36d57f5eb52a722e0f3b8ca5ec20
Alamgir Farouk <afarouk2@hotmail.com>
2016-02-05 21:56:18 -0800

    added mobile_detect php and bug fix
    Added mobile detect php to prepare for combining the /d folder into a
    single folder and do away with the htaccess redirect.
    
    bug fix so that we can show error screen when server is down. TODO
    replace no_sitelette with proper error page.

--- app/index.php
+++ app/index.php
@@ -1,159 +1,183 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+include_once ('Mobile_Detect.php');
 include_once ('parser_api_utility.php');
+
+$detect = new Mobile_Detect;
+ 
+// Any mobile device excluding tablets.
+if( $detect->isMobile() && !$detect->isTablet() ){
+ $desktopIFrame=FALSE;
+ //echo 'mobile phone <br>';
+}else{
+ $desktopIFrame=TRUE;
+ //echo 'desktop computer <br>';
+}
+
 
 $completeURL = full_url($_SERVER, true);
 $serverName = $_SERVER['SERVER_NAME'];
 /* determine the http host */
 if (strpos($serverName, 'localhost') !== false) {
-    $protocol = 'http://';
+	$protocol = 'http://';
 } else {
-    $protocol = 'https://';
+	$protocol = 'https://';
 }
 /*is demo=true */
 if (validateParams('demo')) {
-    $demo = TRUE;
+	$demo = TRUE;
 } else {
-    $demo = FALSE;
+	$demo = FALSE;
 }
 
 /* is desktopiframe=true */
 if (validateParams('desktopiframe')) {
-    $desktop = TRUE;
+	$desktop = TRUE;
 } else {
-    $desktop = FALSE;
+	$desktop = FALSE;
 }
 
 /* is API server specified? */
 if (validateParams('server')) {
-    $server = $_REQUEST['server'];
-    if (strcmp($server, 'localhost') === 0)
-        $server = $server . ':8080';
+	$server = $_REQUEST['server'];
+	if (strcmp($server, 'localhost') === 0)
+		$server = $server . ':8080';
 } else {
-    if ($demo) {
-        $server = "simfel.com";
-    } else {
-        $server = "communitylive.ws";
-    }
+	if ($demo) {
+		$server = "simfel.com";
+	} else {
+		$server = "communitylive.ws";
+	}
 }
 
 /* is IOS embedded specified?*/
 if (validateParams('embedded')) {
-    $embedded = TRUE;
+	$embedded = TRUE;
 } else {
-    $embedded = FALSE;
+	$embedded = FALSE;
 }
 
 /* is serviceAccomodatorId specified (only from Portal) */
 if (validateParams('serviceAccommodatorId')) {
-    $serviceAccommodatorId = $_REQUEST['serviceAccommodatorId'];
+	$serviceAccommodatorId = $_REQUEST['serviceAccommodatorId'];
 } else {
-    $serviceAccommodatorId = NULL;
+	$serviceAccommodatorId = NULL;
 }
 
 if (validateParams('serviceLocationId')) {
-    $serviceLocationId = $_REQUEST['serviceLocationId'];
+	$serviceLocationId = $_REQUEST['serviceLocationId'];
 } else {
-    $serviceLocationId = NULL;
+	$serviceLocationId = NULL;
 }
 
 if (validateParams('friendlyURL')) {
-    $friendlyURL = $_REQUEST['friendlyURL'];
+	$friendlyURL = $_REQUEST['friendlyURL'];
 } else {
-    $friendlyURL = NULL;
+	$friendlyURL = NULL;
 }
 
 if (validateParams('UID')) {
-    $UID = $_REQUEST['UID'];
+	$UID = $_REQUEST['UID'];
 } else {
-    $UID = NULL;
+	$UID = NULL;
 }
 
 /* NOTE: if debug=true then PHP will echo variables and exit */
 if (validateParams('debug')) {
-    $debug = TRUE;
+	$debug = TRUE;
 } else {
-    $debug = FALSE;
+	$debug = FALSE;
 }
 
 if ((!is_null($serviceAccommodatorId)) && (!is_null($serviceLocationId))) {
-    $directAccess = TRUE;
+	$directAccess = TRUE;
 } else {
-    $directAccess = FALSE;
+	$directAccess = FALSE;
 }
 
 if (!is_null($friendlyURL)) {
-    $publicAccess = TRUE;
+	$publicAccess = TRUE;
 } else {
-    $publicAccess = FALSE;
+	$publicAccess = FALSE;
 }
 
 if ($debug) {
-    echo '$completeURL=' . $completeURL . "</br>";
-    echo '$friendlyURL=' . $friendlyURL . "</br>";
-    echo '$server=' . $server . "</br>";
-    echo '$embedded=' . ($embedded ? 'true' : 'false') . "</br>";
-    echo '$demo=' . ($demo ? 'true' : 'false') . "</br>";
-    echo '$serviceAccommodatorId=' . $serviceAccommodatorId . "</br>";
-    echo '$serviceLocationId=' . $serviceLocationId . "</br>";
+	echo '$completeURL=' . $completeURL . "</br>";
+	echo '$friendlyURL=' . $friendlyURL . "</br>";
+	echo '$server=' . $server . "</br>";
+	echo '$embedded=' . ($embedded ? 'true' : 'false') . "</br>";
+	echo '$demo=' . ($demo ? 'true' : 'false') . "</br>";
+	echo '$serviceAccommodatorId=' . $serviceAccommodatorId . "</br>";
+	echo '$serviceLocationId=' . $serviceLocationId . "</br>";
 
-    echo '$UID=' . $UID . "</br>";
-    echo '$directAccess='.$directAccess."</br>";
-    echo '$publicAccess='.$publicAccess."</br>";
+	echo '$UID=' . $UID . "</br>";
+	echo '$directAccess=' . $directAccess . "</br>";
+	echo '$publicAccess=' . $publicAccess . "</br>";
+	echo '$desktopIframe='.$desktopiframe ."</br>";
+	if (!is_null($friendlyURL)) {
+		echo '$friendlyURL is ' . $friendlyURL . "</br>";
+		;
+	} else if ((!is_null($serviceAccommodatorId)) && (!is_null($serviceLocationId))) {
+		echo '$serviceAccommodatorId is ' . $serviceAccommodatorId . ' and $serviceLocationId is ' . $serviceLocationId;
+	} else {
+		echo ' root case ';
+	}
 
-    if (!is_null($friendlyURL)) {
-        echo '$friendlyURL is ' . $friendlyURL . "</br>"; ;
-    } else if ((!is_null($serviceAccommodatorId)) && (!is_null($serviceLocationId))) {
-        echo '$serviceAccommodatorId is ' . $serviceAccommodatorId . ' and $serviceLocationId is ' . $serviceLocationId;
-    } else {
-        echo ' root case ';
-    }
-
-    return;
+	return;
 }
 
 $saslName = NULL;
 $appleTouchIcon60URL = NULL;
 
-
-$isPrivate =FALSE;
+$isPrivate = FALSE;
 $canCreateAnonymousUser = TRUE;
 
-if ($directAccess|| $publicAccess) {
-    if ($publicAccess) {
-        $apiURL = $protocol . $server . "/apptsvc/rest/html/retrieveSiteletteByURLkey?UID=&latitude=&longitude=&urlKey=" . $friendlyURL;
-    } else {
-        $apiURL = $protocol . $server . "/apptsvc/rest/html/retrieveSiteletteBySASL?UID=&latitude=&longitude=&serviceAccommodatorId=" . $serviceAccommodatorId . '&serviceLocationId=' . $serviceLocationId;
-    }
+if ($directAccess || $publicAccess) {
+	if ($publicAccess) {
+		$apiURL = $protocol . $server . "/apptsvc/rest/html/retrieveSiteletteByURLkey?UID=&latitude=&longitude=&urlKey=" . $friendlyURL;
+	} else {
+		$apiURL = $protocol . $server . "/apptsvc/rest/html/retrieveSiteletteBySASL?UID=&latitude=&longitude=&serviceAccommodatorId=" . $serviceAccommodatorId . '&serviceLocationId=' . $serviceLocationId;
+	}
 
-    $siteletteJSON =  makeApiCall($apiURL);
+	$siteletteJSON = makeApiCall($apiURL);
 
-    if (isset($siteletteJSON['error'])) {
+	if ($siteletteJSON['curl_error']) {
+		//echo ' Unable to access services '.$siteletteJSON['curl_error'];
+		/*
+     * TODO
+     * Show error page instead of no_sitelette page
+     */
+		include_once ('../no_sitelette/index.php');
 
-        include_once ('../no_sitelette/index.php');
-    } else {
-        $saslJSON=json_decode($siteletteJSON['saslJSON'],TRUE);
-        $serviceAccommodatorId = $saslJSON['serviceAccommodatorId'];
-        $serviceLocationId = $saslJSON['serviceLocationId'];
-        $saslName = $saslJSON['saslName'];
-        $appleTouchIcon60URL = $saslJSON['appleTouchIcon60URL'];
-        $androidHomeScreenIconURL = $saslJSON['androidHomeScreenIconURL'];
-        if (is_null($friendlyURL)) {
-            if (array_key_exists("anchorURL", $saslJSON)) {
-                $anchorURL = $saslJSON['anchorURL'];
-                if (array_key_exists("friendlyURL", $anchorURL)) {
-                    $friendlyURL = $anchorURL['friendlyURL'];
-                } else {
-                    $friendlyURL = NULL;
-                }
-            } else
-                $friendlyURL = NULL;
-        }
-    }
-    include_once ('sitelette.php');
+	} else {
+
+		if (isset($siteletteJSON['error'])) {
+
+			include_once ('../no_sitelette/index.php');
+		} else {
+			$saslJSON = json_decode($siteletteJSON['saslJSON'], TRUE);
+			$serviceAccommodatorId = $saslJSON['serviceAccommodatorId'];
+			$serviceLocationId = $saslJSON['serviceLocationId'];
+			$saslName = $saslJSON['saslName'];
+			$appleTouchIcon60URL = $saslJSON['appleTouchIcon60URL'];
+			$androidHomeScreenIconURL = $saslJSON['androidHomeScreenIconURL'];
+			if (is_null($friendlyURL)) {
+				if (array_key_exists("anchorURL", $saslJSON)) {
+					$anchorURL = $saslJSON['anchorURL'];
+					if (array_key_exists("friendlyURL", $anchorURL)) {
+						$friendlyURL = $anchorURL['friendlyURL'];
+					} else {
+						$friendlyURL = NULL;
+					}
+				} else
+					$friendlyURL = NULL;
+			}
+		}
+		include_once ('sitelette.php');
+	}
 } else {
-
-    include_once ('../no_sitelette/index.php');
+	/* neither URL nor sa,sl provided */
+	include_once ('../no_sitelette/index.php');
 }
 ?>
