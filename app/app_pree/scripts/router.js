6e10cb9b0f1914766578c7ff9579bbf78cfec0a7
sancraz <sancrazm@gmail.com>
2016-06-04 18:26:12 +0300

    added grunt task, created webpack-pree.config.js, added npm modules into package.json

--- app/app_pree/scripts/router.js
+++ app/app_pree/scripts/router.js
@@ -5,7 +5,7 @@ define(['jquery','backbone','../views/hello'],function($,Backbone, HelloView){
       'about': 'about'
     },
 
-    initialize() {
+    initialize: function() {
       $('#app-content').append('<div id="js-app"></div>').ready(
         function(){
           $('#js-app').hide();
@@ -14,7 +14,7 @@ define(['jquery','backbone','../views/hello'],function($,Backbone, HelloView){
 
     },
 
-    dashboard() {
+    dashboard: function() {
       var helloView = new HelloView().render();
 
       $('#js-app').empty().append(helloView.$el).ready(
@@ -24,7 +24,7 @@ define(['jquery','backbone','../views/hello'],function($,Backbone, HelloView){
       )
     },
 
-    about() {
+    about: function() {
       var helloView = new HelloView({
         template: _.template('Im the about page')
       }).render();
