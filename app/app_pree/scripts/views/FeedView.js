3722591eccc65005fd8003942fa362cfc64331f3
sancraz <sancrazm@gmail.com>
2016-06-13 17:39:47 +0300

    added answersView, tagView, categoryView for pree question, fixed buttons for firefox

--- app/app_pree/scripts/views/FeedView.js
+++ app/app_pree/scripts/views/FeedView.js
@@ -2,7 +2,10 @@
 
 var PreeQuestionView = require('./PreeQuestionView');
 
-var feedView = Backbone.View.extend({
+// var feedView = Backbone.View.extend({
+var feedView = Mn.CollectionView.extend({
+
+    childView: PreeQuestionView,
 
     initialize : function() {
         this.render();
@@ -10,45 +13,45 @@ var feedView = Backbone.View.extend({
     },
 
     modelEventHandler : function() {
-     console.log(" Model event received");
-     this.render();
-    },
-
-    render: function() {
-
-        _(this.model.questionCollection.models).each(function(questionModel) {
-            var el = new PreeQuestionView({
-                model: questionModel,
-                parent: this
-            }).render().el;
-
-            this.$el.find('ul').append(el);
-
-        }.bind(this));
-    //     // var ul;
-
-    //     // if (this.$el.has('ul').length) {
-    //     //     console.log('ul found');
-    //     // } else {
-    //     //     console.log('ul not found');
-    //     //     this.$el.empty();
-    //     //     this.$el.append("<ul></ul>");
-    //     // };
-
-    //     // ul = this.$el.find('ul');
-
-    //     // $(ul[0]).empty();
-
-    //     // this.model.questionCollection.each(function(questionModel) {
-    //     //     console.log(questionModel.get('title'));
-    //     //     $(ul[0]).append('<li style="color:red;"> ' + questionModel.get('title') + "</li>")
-    //     // });
-
-    //     /*
-    //     * if there is no ul, then create one. then, for each item, add a new li from
-    //     * model. Create a pqv with li as el.
-    //     */
+        console.log(" Model event received");
+        this.render();
     }
+
+    // render: function() {
+
+    //     _(this.model.questionCollection.models).each(function(questionModel) {
+    //         var el = new PreeQuestionView({
+    //             model: questionModel,
+    //             parent: this
+    //         }).render().el;
+
+    //         this.$el.find('ul').append(el);
+
+    //     }.bind(this));
+    // //     // var ul;
+
+    // //     // if (this.$el.has('ul').length) {
+    // //     //     console.log('ul found');
+    // //     // } else {
+    // //     //     console.log('ul not found');
+    // //     //     this.$el.empty();
+    // //     //     this.$el.append("<ul></ul>");
+    // //     // };
+
+    // //     // ul = this.$el.find('ul');
+
+    // //     // $(ul[0]).empty();
+
+    // //     // this.model.questionCollection.each(function(questionModel) {
+    // //     //     console.log(questionModel.get('title'));
+    // //     //     $(ul[0]).append('<li style="color:red;"> ' + questionModel.get('title') + "</li>")
+    // //     // });
+
+    // //     /*
+    // //     * if there is no ul, then create one. then, for each item, add a new li from
+    // //     * model. Create a pqv with li as el.
+    // //     */
+    // }
 });
 
 module.exports = feedView;
