baafad1abbdc44438aaf1bf363ae205aefa07d52
Alamgir Farouk <afarouk2@hotmail.com>
2016-06-26 22:59:01 -0700

    Added 'disabled editing' of combo items
    If an item type is ‘COMBO’ we do not allow the item to be removed from
    the basket.

--- app/app_sitelette/scripts/views/panels/editFavoritesView.js
+++ app/app_sitelette/scripts/views/panels/editFavoritesView.js
@@ -2,25 +2,20 @@
 
 'use strict';
 
-var template = require('ejs!../../templates/editFavorites.ejs'),
-    loader = require('../../loader'),
-    PanelView = require('../components/panelView'),
-    ListView = require('../components/listView'),
-    EditFavoriteItem = require('../partials/edit_favorite_item'),
-    h = require('../../globalHelpers');
+var template = require('ejs!../../templates/editFavorites.ejs'), loader = require('../../loader'), PanelView = require('../components/panelView'), ListView = require('../components/listView'), EditFavoriteItem = require('../partials/edit_favorite_item'), h = require('../../globalHelpers');
 
 var EditFavoritesView = PanelView.extend({
 
-    template: template,
+    template : template,
 
-    addedEvents: {
-        'click .back_button': 'openSettings',
-        'click .cmntyex-button-edit': 'toggleEditable',
-        'click .cmntyex-button-cancel': 'toggleEditable',
-        'click .cmntyex-button-done': 'removeSelected'
+    addedEvents : {
+        'click .back_button' : 'openSettings',
+        'click .cmntyex-button-edit' : 'toggleEditable',
+        'click .cmntyex-button-cancel' : 'toggleEditable',
+        'click .cmntyex-button-done' : 'removeSelected'
     },
 
-    initialize: function (options){
+    initialize : function(options) {
 
         options = options || {};
 
@@ -28,55 +23,86 @@ var EditFavoritesView = PanelView.extend({
 
         this.actions = options.actions;
 
-        this.$el.attr({'id': 'cmntyex_edit_favorites_panel'});
+        this.$el.attr({
+            'id' : 'cmntyex_edit_favorites_panel'
+        });
 
         this.addEvents(this.addedEvents);
 
-        this.collection.each(function (model) {
+        this.collection.each(function(model) {
+            var editable = true;
+            var combo = false;
+            if (model.attributes){ 
+                if (model.attributes.itemType){ 
+                    if (model.attributes.itemType.enumText === 'COMBO') {
+                        editable = false;
+                        combo = true;
+                    }
+                }
+            }
+            ;
+
             model.set({
-                'editable': true,
-                'selected': false
+                'editable' : editable,
+                'selected' : combo,
+                'combo' : true
             });
         });
 
     },
 
-    render: function (update) {
-        //prevent jqm panel widget from breaking when updated
+    render : function(update) {
+        // prevent jqm panel widget from breaking when updated
         var container = update ? this.$('.ui-panel-inner') : this.$el;
         var editable = this.collection.length > 0 ? this.collection.at(0).get('editable') : false;
-        container.html(this.template({ editable: editable }));
+        container.html(this.template({
+            editable : editable
+        }));
         container.find('.cmntyex-list_container').html(new ListView({
-            collection: this.collection,
-            ListItemView: EditFavoriteItem,
-            ListItemViewOptions: {
-                template: this.itemTemplate
+            collection : this.collection,
+            ListItemView : EditFavoriteItem,
+            ListItemViewOptions : {
+                template : this.itemTemplate
             },
-            parent: this
+            parent : this
         }).render().el);
         return this;
     },
 
-    toggleEditable: function () {
-        this.collection.each(function (model) {
-            model.set('editable', !model.get('editable'));
+    toggleEditable : function() {
+        this.collection.each(function(model) {
+            var editable = true;
+            var combo = false;
+            if (model.attributes){
+                if (model.attributes.itemType){ 
+                    if (model.attributes.itemType.enumText === 'COMBO') {
+                        editable = false;
+                        combo = true;
+                    }
+                }
+            }
+            ;
+            if (!combo) {
+                model.set('editable', !model.get('editable'));
+            }
         });
         this._update();
     },
 
-    removeSelected: function () {
+    removeSelected : function() {
         loader.show('deleting items');
-        var selected = this.collection.where({selected: true});
-        $.when(this.actions.removeItem(selected))
-            .then(function () {
-                this.toggleEditable();
-                loader.hide();
-            }.bind(this), function () {
-                loader.showFlashMessage(h().getErrorMessage(e, 'error deleting favorites'));
-            });
+        var selected = this.collection.where({
+            selected : true
+        });
+        $.when(this.actions.removeItem(selected)).then(function() {
+            this.toggleEditable();
+            loader.hide();
+        }.bind(this), function() {
+            loader.showFlashMessage(h().getErrorMessage(e, 'error deleting favorites'));
+        });
     },
 
-    _update: function () {
+    _update : function() {
         this.render(true);
         this.enhance();
     }
