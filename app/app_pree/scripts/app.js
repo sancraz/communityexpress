6b212f0b86ece1ae2b58af901250fb8f0ad0abeb
sancraz <sancrazm@gmail.com>
2016-07-20 20:52:12 +0300

    fixed gruntfile for sitelette

--- app/app_pree/scripts/app.js
+++ app/app_pree/scripts/app.js
@@ -18,6 +18,18 @@ App.goToPage = function(viewName) {
 App.on('viewChange', App.goToPage);
 
 App.on('start',function() {
+    $('#app-container').on('click', 'a[href]:not([data-bypass])', function(evt) {
+        // Get the absolute anchor href.
+        var href = { prop: $(this).prop('href'), attr: $(this).attr('href') };
+        // Get the absolute root.
+        var root = location.protocol + '//' + location.host;
+
+        // Ensure the root is part of the anchor href, meaning it's relative.
+        if (href.prop.slice(0, root.length) === root) {
+            evt.preventDefault();
+            // Backbone.history.navigate(href.attr, true);
+        }
+    });
 
     this.params = window.community;
 
