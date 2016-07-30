73a26192029f8ae10d9236cd43e16ac3c0602a3b
Alamgir Farouk <afarouk2@hotmail.com>
2016-07-30 03:23:51 -0700

    placeholder foot

---
+++ app/app_pree/pages/footer.php
@@ -0,0 +1,34 @@
+<div class="container " style="background-color:#388e3c; color:white; width:100%; margin-left:0px;padding-top:20px;margin-right:0px;"  >
+<footer id="footer">
+ <section class="container footer-in">
+  <div class="col-md-3">
+   <div class="widget">
+    <div class="menu-footer1-container">
+     <ul class="menu">
+      <li> About Us </li>
+      <li> Contact Us </li>
+     </ul>
+    </div>
+   </div>
+  </div>
+  <div class="col-md-3">
+   <div class="widget">
+    <div class="menu-footer4-container">
+     <ul class="menu">
+      <li> Privacy Policy  </li>
+      <li> Terms &amp; Conditions </li>
+     </ul>
+    </div>
+   </div>
+  </div>
+   <div class="col-md-offset-3 col-md-3">
+   <div style="text-align: left;" class="col-md-12">
+
+   <img id="footer-logo" src="/images/Pree.png" alt="pree
+                small logo">  &#0169 &nbsp;2016. <br> All rights reserved. 
+   </div>
+  </div>
+ </section>
+ <!-- end-footbot -->
+</footer>
+</div>
