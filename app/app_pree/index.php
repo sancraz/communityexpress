720052696d86d668f65f74417a641766cc100c15
Alamgir Farouk <afarouk2@hotmail.com>
2016-08-14 04:36:03 -0700

    sharing preparation

--- app/app_pree/index.php
+++ app/app_pree/index.php
@@ -4,6 +4,12 @@
 <!DOCTYPE html>
 <html lang="en" class="js flexbox flexboxlegacy canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths ui-mobile">
  <head>
+ <!-- og meta tags --> 
+<meta property="og:url"                content="<?PHP echo $og_url?>"/>
+<meta property="og:type"               content="article" />
+<meta property="og:title"              content="<?PHP echo $og_title?>"/>
+<meta property="og:description"        content="<?PHP echo $og_description?>"/>
+<meta property="og:image"              content="<?PHP echo $og_image?>"/>
   <!-- Title here -->
  <title>Pree - Home</title>
  <?php
